9c5b22fdcbc363e6367149b02c5c3b7c96054862
Mustapha <mustapha.aouadi2@gmail.com>
2024-09-12 22:20:31 +0300

    first commitB

--- test/register_page_test.dart
+++ test/register_page_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
 import 'package:travel_app/controller/RegisterController.dart';
-import 'package:travel_app/register/register.dart';  // Ensure correct path
+import 'package:travel_app/Register/register.dart';  // Ensure correct path
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';  // Import this for TextFormField
 
