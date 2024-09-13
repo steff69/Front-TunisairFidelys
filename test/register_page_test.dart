436ba760e7693c475faac40d63e175aca977089a
Mustapha <mustapha.aouadi2@gmail.com>
2024-09-13 04:40:18 +0300

    first commitB

--- test/register_page_test.dart
+++ test/register_page_test.dart
@@ -1,15 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
-import 'package:travel_app/Register/register.dart';  // Ensure correct path
+import 'package:travel_app/Register/register.dart';
 import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
 import 'package:flutter/material.dart';
+import 'package:mockito/annotations.dart';  // Make sure this is added
 
-class MockClient extends Mock implements http.Client {}
-
+@GenerateMocks([http.Client])
 void main() {
   late MockClient mockClient;
 
   setUp(() {
+    Get.testMode = true;  // This prevents GetX navigation issues during testing
     mockClient = MockClient();
   });
 
