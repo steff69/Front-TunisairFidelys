4559acf6ba8bcbe36d835a6456e8c680b3160849
Mustapha <mustapha.aouadi2@gmail.com>
2024-09-13 05:48:40 +0300

    first commitB

--- lib/Register/register.dart
+++ lib/Register/register.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:travel_app/Models%202/LoginModel.dart';
+import 'package:travel_app/Models%202/RegisterModel.dart';
 import 'package:travel_app/common/EmailTextField.dart';
 import 'package:travel_app/common/PasswordTextField.dart';
+import 'package:travel_app/common/back_ground_container.dart';
 import 'package:travel_app/common/shimmers/cutomButtomn.dart';
 import 'package:travel_app/constants/constants.dart';
+import 'package:travel_app/controller/LoginController.dart';
 import 'package:travel_app/controller/RegisterController.dart';
-import 'package:travel_app/Models%202/RegisterModel.dart';
-import 'package:http/http.dart' as http;
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -20,7 +24,9 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   late final TextEditingController _emailController = TextEditingController();
   late final TextEditingController _nameController = TextEditingController();
-  late final TextEditingController _passwordController = TextEditingController();
+
+  late final TextEditingController _passwordController =
+      TextEditingController();
   final FocusNode _passwordCFocusNode = FocusNode();
 
   @override
@@ -29,13 +35,13 @@ class _RegisterPageState extends State<RegisterPage> {
     _passwordController.dispose();
     _passwordCFocusNode.dispose();
     _nameController.dispose();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // Injecting the real http.Client here
-    var c = Get.put(RegisterController(client: http.Client()));
+    var c = Get.put(RegisterController());
 
     return Scaffold(
       backgroundColor: kwhite,
@@ -43,14 +49,12 @@ class _RegisterPageState extends State<RegisterPage> {
         elevation: 0,
         backgroundColor: kwhite,
         title: Padding(
-          padding: EdgeInsets.only(left: 100),
-          child: Text(
-            'Sign Up',
-            style: TextStyle(
-              color: kDark, 
-              fontSize: 20, 
-              fontWeight: FontWeight.bold),
-          )),
+            padding: EdgeInsets.only(left: 100),
+            child: Text(
+              'Sign Up',
+              style: TextStyle(
+                  color: kDark, fontSize: 20, fontWeight: FontWeight.bold),
+            )),
       ),
       body: Container(
         width: widthh,
@@ -59,9 +63,13 @@ class _RegisterPageState extends State<RegisterPage> {
           borderRadius: BorderRadius.only(),
           child: ListView(
             children: [
-              SizedBox(height: 5.h),
+              SizedBox(
+                height: 5.h,
+              ),
               Image.asset('assets/t.jpg'),
-              SizedBox(height: 35.h),
+              SizedBox(
+                height: 35.h,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Column(
@@ -75,7 +83,9 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                       controller: _emailController,
                     ),
-                    SizedBox(height: 20.h),
+                    SizedBox(
+                      height: 20.h,
+                    ),
                     EmailTextField(
                       hintText: "Name",
                       prefixIcon: Icon(
@@ -85,11 +95,15 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                       controller: _nameController,
                     ),
-                    SizedBox(height: 20.h),
+                    SizedBox(
+                      height: 20.h,
+                    ),
                     PasswordTextField(
                       controller: _passwordController,
                     ),
-                    SizedBox(height: 20.h),
+                    SizedBox(
+                      height: 20.h,
+                    ),
                     Obx(() => c.loading.value
                         ? Center(
                             child: CircularProgressIndicator(
@@ -105,7 +119,7 @@ class _RegisterPageState extends State<RegisterPage> {
                               String data = RegisterModelToJson(model);
                               c.registerFunction(data);
                             },
-                            text: "SIGN UP",
+                            text: "SING UP",
                             btnheigh: 35.h,
                           )),
                   ],
